ed975e446c9ebe38487238259212bdeb0c9f677d
AxiosGao <522492107@qq.com>
2018-04-28 13:37:43 +0800

    use_agent

--- spider_lagou.py
+++ spider_lagou.py
@@ -1,5 +1,6 @@
 import time
 import json
+import random
 
 from urllib import request
 from urllib.request import quote
@@ -7,25 +8,24 @@ from bs4 import BeautifulSoup
 from urllib import parse
 
 
-def get_content(url, city, district):
+def get_content(url, city, district, user_agent):
     city = quote(city)
     district = quote(district)
     referer_url = "https://www.lagou.com/jobs/list_PHP?px=default&city=" + \
         city + "&district=" + district
-    # 设置header参数
-    header = {'Accept': 'text/html, application/xhtml+xml, image/jxr, */*; q=0.01',
-              'Accept - Encoding': 'gzip, deflate, br',
-              'Accept-Language': 'zh-Hans-CN, zh-Hans; q=0.5',
-              'Cache-Control': 'no-cache',
-              'Connection': 'Keep-Alive',
-              'Referer': referer_url, # 拉钩接口必须要有的参数
-              'Cookie': 'JSESSIONID=ABAAABAAAFCAAEGE8FFB426BFB1AA73B63B9C41B2B3DAD9;',
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
-              'Origin': 'https://www.lagou.com',
-              }
-
     i = 1
     while i < 10:
+        # 设置header参数
+        header = {'Accept': 'text/html, application/xhtml+xml, image/jxr, */*; q=0.01',
+                'Accept - Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'zh-Hans-CN, zh-Hans; q=0.5',
+                'Cache-Control': 'no-cache',
+                'Connection': 'Keep-Alive',
+                'Referer': referer_url, # 拉钩接口必须要有的参数
+                # 'Cookie': 'JSESSIONID=ABAAABAACBHABBI20ECE091E355B972DD12DB195885E67A;',
+                'User-Agent': random.choice(user_agent),
+                'Origin': 'https://www.lagou.com',
+                }
         body = parse.urlencode(
             {"first": "false", "pn": i, "kd": "PHP"}).encode(encoding="UTF8")
         res = request.Request(url, data=body, headers=header)
@@ -62,5 +62,22 @@ input_district = input("请输入要查找的区：")
 # input_city = "成都"
 # input_district = "高新区"
 
+USERAGENT=[
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6',
+    'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/44.0.2403.89 Chrome/44.0.2403.89 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+    'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+]
+
 url = get_url(input_city, input_district)
-get_content(url, input_city, input_district)
+get_content(url, input_city, input_district, USERAGENT)
