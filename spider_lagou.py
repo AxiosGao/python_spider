00457c890f207fa766235d0c5420c7521ad4214c
AxiosGao <522492107@qq.com>
2018-04-28 10:55:50 +0800

    update

--- spider_lagou.py
+++ spider_lagou.py
@@ -1,4 +1,4 @@
-import re
+import time
 import json
 
 from urllib import request
@@ -11,23 +11,23 @@ def get_content(url, city, district):
     city = quote(city)
     district = quote(district)
     referer_url = "https://www.lagou.com/jobs/list_PHP?px=default&city=" + \
-                  city + "&district=" + district
+        city + "&district=" + district
     # 设置header参数
     header = {'Accept': 'text/html, application/xhtml+xml, image/jxr, */*; q=0.01',
               'Accept - Encoding': 'gzip, deflate, br',
               'Accept-Language': 'zh-Hans-CN, zh-Hans; q=0.5',
               'Cache-Control': 'no-cache',
               'Connection': 'Keep-Alive',
-              'Referer': referer_url,  # 拉钩接口必须要有的参数
+              'Referer': referer_url, # 拉钩接口必须要有的参数
               'Cookie': 'JSESSIONID=ABAAABAAAFCAAEGE8FFB426BFB1AA73B63B9C41B2B3DAD9;',
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
               'Origin': 'https://www.lagou.com',
               }
 
-    i = 0
+    i = 1
     while i < 10:
         body = parse.urlencode(
-            {"first": "false", "pn": 1, "kd": "PHP"}).encode(encoding="UTF8")
+            {"first": "false", "pn": i, "kd": "PHP"}).encode(encoding="UTF8")
         res = request.Request(url, data=body, headers=header)
         html = request.urlopen(res)
         if html:
@@ -37,13 +37,14 @@ def get_content(url, city, district):
                 i += 1
                 continue
             for info in bs_json["content"]["positionResult"]["result"]:
-                company = info['companyFullName']  # 公司名
-                position = info['positionName']  # 职位名称
-                work_year = info['workYear']  # 工作年限
-                education = info['education']  # 教育背景
-                salary = info['salary']  # 薪资
-                print(company + "  |  " + position + "  |  " + work_year + "  |  " + education + "  |  " + salary)
+                company = info['companyFullName'] # 公司名
+                position = info['positionName'] # 职位名称
+                work_year = info['workYear'] # 工作年限
+                education = info['education'] # 教育背景
+                salary = info['salary'] # 薪资
+                print(company+"  |  "+position+"  |  "+work_year+"  |  "+education+"  |  "+salary)
                 print("============================================================")
+            time.sleep(1)
         i += 1
 
 
@@ -51,7 +52,7 @@ def get_url(city, district):
     city = quote(city)
     district = quote(district)
     url = "https://www.lagou.com/jobs/positionAjax.json?px=default&city=" + \
-          city + "&district=" + district + "&needAddtionalResult=false"
+        city + "&district=" + district + "&needAddtionalResult=false"
     return url
 
 
